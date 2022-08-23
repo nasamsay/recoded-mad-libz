01776c9cf7677d2589897fa3bd3d280ea6d095ba
Nasam Sayour <nasamsay@gmail.com>
2022-08-23 20:00:27 +0300

    commit #1

--- madlibs.js
+++ madlibs.js
@@ -27,8 +27,36 @@
  * Please go through this lesson: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/
  */
 function parseStory(rawStory) {
-  // Your code here.
-  return {}; // This line is currently wrong :)
+  //console.log(rawStory)
+  let input = rawStory.split(/\s|]/)
+  //console.log(input)
+  const verbRex = /[v]$/
+  const nounRex = /[n]$/
+  const adjRex = /[a]$/
+  const output = []
+  for(i =0; i<input.length; i++){
+    let obj ={}
+    if(verbRex.test(input[i])){
+      obj['word'] = input[i]
+      obj['pos'] ='verb'
+      output.push(obj)
+    }
+    else if(nounRex.test(input[i])){
+      obj['word'] = input[i]
+      obj['pos'] ='noun'
+      output.push(obj)
+    }
+    else if(adjRex.test(input[i])){
+      obj['word'] = input[i]
+      obj['pos'] ='adjective'
+      output.push(obj)
+    }
+    else{
+      obj['word'] = input[i]
+      output.push(obj)
+    }
+  }
+  return output;
 }
 
 /**
@@ -42,6 +70,25 @@ function parseStory(rawStory) {
  *
  * You'll want to use the results of parseStory() to display the story on the page.
  */
+function editStory(obj){
+  const editText = document.querySelector('div.madLibsEdit')
+  for(i=0; i<obj.length; i++){
+    if(obj[i].pos == 'verb'){
+      editText.innerHTML= editText.innerHTML+'<input type="text"></input>'+'[v]'
+    }
+    else if(obj[i].pos == 'noun'){
+      editText.innerHTML= editText.innerHTML+'<input type="text"></input>'+'[n]'
+    }
+    else if(obj[i].pos == 'adjective'){
+      editText.innerHTML= editText.innerHTML+'<input type="text"></input>'+'[a]'
+    }
+    else{
+      editText.innerHTML = editText.innerHTML+obj['word']
+    }
+  }
+}
+ 
+ //const preview = document.querySelector('div.madLibsPreview')
 getRawStory()
   .then(parseStory)
   .then((processedStory) => {
